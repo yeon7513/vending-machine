1d126890bf7c49edae72a6fd32fe1ff45e513a5f
Flowipe <yeon7513@nate.com>
2024-05-24 17:34:15 +0900

    20240524

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+`
+<li class="item before">
+<i class="fa-solid icon"></i>
+<p>레모네이드</p>
+<div class="btn-wrap">
+  <button id="plus" class="btn qty-btn">+</button>
+  <span id="qty-result">1</span>
+  <button id="minus" class="btn qty-btn">-</button>
+  <button id="delete-btn" class="btn">삭제</button>
+</div>
+</li>
+`;
