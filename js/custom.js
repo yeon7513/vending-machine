3141cf3ce4d44686c76d2f02d8c413dfb3fe635d
yeon7513 <yeon7513@nate.com>
2024-07-10 17:29:10 +0900

    20240710

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,5 @@
 'use strict';
 
-let possessedMoney = 25000;
-let insertMoney = 0;
-let totalPrice;
-let totalQty;
-let cart = [];
-
 const productArr = [
   { icon: 'lemon', name: '레모네이드', qty: 5, price: 1800 },
   { icon: 'apple-whole', name: '사과주스', qty: 5, price: 2000 },
@@ -22,21 +16,26 @@ const cartList = document.getElementById('cart-list');
 const cartCost = document.querySelector('#cart-cost > span');
 const purchaseList = document.getElementById('purchase-list');
 
+let possessedMoney = 25000;
+let insertMoney = 0;
+let cartArr = [];
+let totalPrice;
+
 possession.innerHTML = possessedMoney;
 remainingAmount.innerHTML = insertMoney;
 
 // 음료수 출력
+
 productArr.forEach((item) => {
   products.innerHTML += `
-    <div class="product-item bg">
-      <i class="fa-solid fa-${item.icon}"></i>
-      <p class="product-name">${item.name}</p>
-      <p class="product-qty">수량 : <span>${item.qty}</span>개</p>
-      <button class="btn product-btn" data-product="${item.icon}" data-price="${item.price}">${item.price}원</button>
-    </div>
-  `;
+      <div class="product-item bg">
+        <i class="fa-solid fa-${item.icon}"></i>
+        <p class="product-name">${item.name}</p>
+        <p class="product-qty">수량 : ${item.qty}개</p>
+        <button class="btn product-btn" data-product="${item.icon}" data-price="${item.price}">${item.price}원</button>
+      </div>
+    `;
 });
-
 // 자판기에 돈 추가하기
 function addCash(cash) {
   if (possessedMoney == 0 || possessedMoney < cash) {
@@ -54,34 +53,6 @@ function getChange() {
   insertMoney = 0;
   remainingAmount.innerHTML = insertMoney;
 }
-
-// 각 제품들의 버튼 관리
-const productBtn = document.querySelectorAll('.product-btn');
-
-productBtn.forEach((e) => {
-  let countQty = 0;
-  const productQty = e.previousElementSibling;
-  const productQtyChild = productQty.firstElementChild;
-  e.addEventListener('click', function () {
-    const product = e.getAttribute('data-product');
-    const price = parseInt(e.getAttribute('data-price'));
-
-    const productObj = productArr.find((item) => item.icon === product);
-
-    if (productObj.qty > 0) {
-      countQty++;
-      if (productObj.qty > countQty) {
-        productQtyChild.innerHTML = productObj.qty - countQty;
-        addCart(productObj, price);
-      } else if (productObj.qty == countQty) {
-        productQty.innerHTML = '품절';
-      }
-    } else {
-      alert('품절된 상품입니다.');
-      return false;
-    }
-  });
-});
 
 // 제품 이름 가져오기
 function getProductName(product) {
@@ -103,28 +74,56 @@ function getProductName(product) {
   }
 }
 
-// 장바구니에 선택한 제품 추가
-function addCart(product, price) {
-  const cartItem = cart.find((item) => item.icon === product.icon);
+const productBtns = document.querySelectorAll('.product-btn');
+
+productBtns.forEach((btn) => {
+  const productQty = btn.previousElementSibling;
+
+  // 각 제품을 클릭하면 장바구니에 들어간다.
+  btn.addEventListener('click', function () {
+    const productName = btn.getAttribute('data-product');
+    // const price = parseInt(btn.getAttribute('data-price'));
+
+    const productObj = productArr.find((item) => item.icon === productName);
+
+    if (productObj.qty > 0) {
+      // => 제품을 클릭한 횟수 만큼 수량이 차감된다.
+      let calc = productObj.qty - 1;
+      productObj.qty = calc;
+      productQty.innerHTML = `수량 : ${calc}개`;
+
+      // => 장바구니에 들어간 제품의 갯수만큼 금액이 합산된다.
+
+      // => 수량이 0개가 되면 '품절'된다.
+      if (productObj.qty == 0) {
+        productQty.innerHTML = '품절';
+      }
+    } else {
+      alert('품절된 상품입니다.');
+      return false;
+    }
+    console.log('productObj: ', productObj);
+    addCart(productObj);
+  });
+});
+
+function addCart(product) {
+  const cartItem = cartArr.find((item) => item.icon === product.icon);
 
   if (cartItem) {
     cartItem.qty++;
   } else {
-    cart.push({ ...product, qty: 1 });
+    cartArr.push({ ...product, qty: 1 });
   }
-  console.log('product', product);
-  console.log('cart', cart);
-  product.qty--;
+  // console.log('cartArr: ', cartArr);
+  // console.log('cartItem: ', cartItem);
   cartUpdate();
 }
 
-// 장바구니 목록 업데이트
 function cartUpdate() {
   cartList.innerHTML = '';
   totalPrice = 0;
-  totalQty = 0;
-  cart.forEach((item, idx) => {
-    console.log(idx);
+  cartArr.forEach((item, idx) => {
     cartList.insertAdjacentHTML(
       'beforeend',
       `
@@ -142,20 +141,62 @@ function cartUpdate() {
         </li>
       `
     );
+
+    // console.log('item.qty: ', item.qty);
     totalPrice += item.price * item.qty;
-    totalQty += item.qty;
   });
   cartList.scrollTop = cartList.offsetHeight;
-  cartList.nextElementSibling.firstElementChild.innerHTML = totalPrice;
+  cartCost.innerHTML = totalPrice;
 }
 
-// 구매 목록 업데이트
+// 장바구니 안에서 버튼을 조작할 수 있다.
+// => +버튼은 장바구니에 있는 수량이 증가하고, 음료수 부분에서의 수량은 감소한다.
+// => -버튼은 장바구니에 있는 수량이 감소하고, 음료수 부분에서의 수량은 증가한다.
+// => 삭제 버튼을 누르면 장바구니에 들어간 제품이 삭제되고, 음료수 부분의 수량이 원래대로 돌아간다.
+// => +버튼을 눌러서 음료수 수량이 0개가 되면 '품절'된다.
+// => -버튼을 눌러서 장바구니 안에서의 수량이 0개가 되면 장바구니 목록에서 삭제된다.
+cartList.addEventListener('click', (e) => {
+  // console.log(e.target);
+  const liEl = e.target.closest('li.cart-item');
+
+  if (!liEl) return;
+
+  const idx = parseInt(liEl.dataset.index);
+
+  const plusBtn = e.target.classList.contains('plus-btn');
+  const minusBtn = e.target.classList.contains('minus-btn');
+  const deleteBtn = e.target.classList.contains('delete-btn');
+
+  if (plusBtn) {
+    console.log(plusBtn);
+    updateCart(idx, 1);
+  }
+
+  // console.log(idx);
+});
+
+function updateCart(idx, change) {
+  const item = cartArr[idx];
+  const product = productArr.find((product) => product.icon === item.icon);
+
+  if (change > 0 && product.qty > 0) {
+    item.qty += 1;
+    product.qty -= 1;
+    console.log('item++ : ', item);
+    console.log('product-- : ', product);
+  }
+  cartUpdate();
+}
+
+// 구매 버튼을 누르면
+// => 장바구니 목록에 있던 아이템들이 구매 목록으로 넘어간다.
+// => 장바구니 목록은 비워지고 남은 금액에서 합계금액을 뺀다.
 function purchaseUpdate() {
   if (insertMoney < totalPrice) {
     alert('남은 금액보다 장바구니 금액이 큽니다.');
     return false;
   } else {
-    cart.forEach((item) => {
+    cartArr.forEach((item) => {
       purchaseList.insertAdjacentHTML(
         'beforeend',
         `
@@ -172,71 +213,8 @@ function purchaseUpdate() {
 
     remainingAmount.innerHTML = insertMoney -= totalPrice;
     cartList.innerHTML = '<li>물품을 추가해 주세요.</li>';
-    cart = [];
+    cartArr = [];
     totalPrice = 0;
-    cartList.nextElementSibling.firstElementChild.innerHTML = totalPrice;
+    cartCost.innerHTML = totalPrice;
   }
 }
-
-// 장바구니 안에 있는 버튼 기능 추가
-cartList.addEventListener('click', (event) => {
-  const liEl = event.target.closest('li.cart-item');
-
-  if (!liEl) return;
-  const idx = parseInt(liEl.dataset.index);
-
-  if (event.target.classList.contains('plus-btn')) {
-    updateQty(idx, 1);
-  } else if (event.target.classList.contains('minus-btn')) {
-    updateQty(idx, -1);
-  } else if (event.target.classList.contains('delete-item')) {
-    removeItem(idx);
-  }
-});
-
-function updateQty(index, change) {
-  const item = cart[index];
-  const product = productArr.find((product) => product.icon === item.icon);
-
-  if (change > 0 && product.qty > 0) {
-    item.qty += change;
-    product.qty -= change;
-  } else if (change < 0 && item.qty > 0) {
-    item.qty += change;
-    product.qty -= change;
-  }
-
-  if (item.qty <= 0) {
-    cart.splice(index, 1);
-  }
-
-  cartUpdate();
-  updateProductQtyDisplay(item.icon, product.qty);
-}
-
-function removeItem(index) {
-  const item = cart[index];
-  const product = productArr.find((product) => product.icon === item.icon);
-
-  console.log(item);
-
-  product.qty += item.qty;
-
-  cart.splice(index, 1);
-
-  cartUpdate();
-  updateProductQtyDisplay(item.icon, product.qty);
-}
-
-function updateProductQtyDisplay(productIcon, qty) {
-  const productItem = products
-    .querySelector(`.product-btn[data-product="${productIcon}"]`)
-    .closest('.product-item');
-
-  const productQty = productItem.querySelector('.product-qty');
-  productQty.innerHTML = `수량 : <span>${qty}</span>개`;
-
-  console.log(productQty);
-}
-
-cartUpdate();
